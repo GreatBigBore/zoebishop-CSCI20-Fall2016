e9798ed5d4781cf7d86c76c0efff3ce42f4295aa
Zoe Bishop <zbishop001@student.butte.edu>
2016-08-25 20:41:15 +0000

    lab 1.1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+cout << "CCCC  AAAA  TTTTT" << endl;
+cout << "C     A  A    T" << endl;
+cout << "C     AAAA    T" << endl;
+cout << "C     A  A    T" << endl;
+cout << "CCCC  A  A    T" << endl;
+
+    return 0;
+}
