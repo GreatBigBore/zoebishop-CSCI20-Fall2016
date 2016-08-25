99ad4372d7a6880f31984d2907b17fde178ecadc
Zoe Bishop <zbishop001@student.butte.edu>
2016-08-25 21:00:32 +0000

    Lab 2 Submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,10 @@
+//Guessing Game Algorithm
+
+//Computer generates number between 1-10 (save as variable numComputer)
+//Input - User enters number (save as variable numUser)  
+//If numComputer = numUser, display message "You win!" 
+//add 10 to variable numPoints (variable initially starts at value of 0)
+//If numComputer doesn't equal numUser, display message "Guess higher" if numComputer is > numUser, or "Guess lower" if numCOmputer is < numUser
+//User enters new number, saved as new value for numUser
+//User enters new number until numComputer = numUser
+//Subtract 1 from numPoints for every time numComputer doesn't equal numUser
