49712a1436aa4c9216ac364c2c2fff5058f34ac8
Zoe Bishop <zbishop001@student.butte.edu>
2016-10-11 23:04:51 +0000

    lab13b submission

--- lab13b/lab13b.cpp
+++ lab13b/lab13b.cpp
@@ -11,39 +11,58 @@ using namespace std;
 int main(){
     
     int currentTimeHour;
-    int currentTimeMin;
+    int currentTimeMin = 0;
 
     cout << "Here is a list of every minute between 1:00 and 2:59: " << endl;
     
    //outputs minutes from 1:00 - 1:59
-    for (currentTimeHour = 1; currentTimeHour <= 2 && currentTimeMin <= 60; ++currentTimeMin){
-        cout << currentTimeMin << endl;
+    for (currentTimeHour = 1; currentTimeHour <= 2 && currentTimeMin < 60; ++currentTimeMin){
+        if (currentTimeMin < 10){
+             cout << "1:0" << currentTimeMin << endl;
+        }
+        else{
+        cout << "1:" << currentTimeMin << endl;
+        }
     }
     
     //resets minute value and outputs minutes from 2:00 = 2:59
-    currentTimeMin = 0;
-    for (currentTimeHour = 2; currentTimeHour <= 2 && currentTimeMin <= 60; ++currentTimeMin){
-        cout << currentTimeMin << endl;
+    for (currentTimeMin = 0; currentTimeHour <= 2 && currentTimeMin < 60; ++currentTimeMin){
+        if (currentTimeMin < 10){
+            cout << "2:0" << currentTimeMin << endl;
+        }
+        else{
+        cout << "2:" << currentTimeMin << endl;
+        }
     }
     
     int startTimeHour;
     int endTimeHour;
+    int startTimeMin;
+    int endTimeMin;
     
     //accepts user input for hour values
     cout << "Please input a start time hour value between 1 and 12." << endl;
     cin >> startTimeHour;
+    cout << "Please input a start time hour value between 0 and 59." << endl;
+    cin >> startTimeMin;
     cout << endl << "Please input an end time hour value between 1 and 12." << endl;
     cin >> endTimeHour;
-    
-    currentTimeHour = startTimeHour;
+    cout << endl << "Please input an end time hour value between 0 and 59." << endl;
+    cin >> endTimeMin;
+    cout << endl;
     
     //outputs 15 minutes intervals between user-specified hours
      for (currentTimeHour = startTimeHour; currentTimeHour <= endTimeHour; ++currentTimeHour){
-        currentTimeMin = 0;
-        cout << endl << currentTimeMin << endl;
-        cout << currentTimeMin + 15 << endl;
-        cout << currentTimeMin + 30 << endl;
-        cout << currentTimeMin + 45 << endl;
-        cout << currentTimeMin + 60 << endl;
+        for (currentTimeMin = startTimeMin; currentTimeMin < 60; currentTimeMin = currentTimeMin + 15){
+            if (currentTimeMin == 0){
+                cout << currentTimeHour << ":0" << currentTimeMin << endl;
+            }
+            else{
+            cout << currentTimeHour << ":" << currentTimeMin << endl;
+            }
+        }
+        if (currentTimeMin == 60){
+            currentTimeMin = 0;
+        }
     }
 }
