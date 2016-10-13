3779dad861005a3363079d33298073edb79570c6
Zoe Bishop <zbishop001@student.butte.edu>
2016-10-13 20:19:43 +0000

    lab11 submission

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -21,20 +21,25 @@ int main(){
     cout << endl;
   
     if (maritalStatus == "no"){
+        adjustedIncome = adjustedIncome - 3900 - 6100;
+        if (adjustedIncome > 0){
         if (adjustedIncome <= 8925){
             correctTax = adjustedIncome * .1;
         }
         else if (adjustedIncome <= 36250){
             correctTax = (adjustedIncome - 8925) * .15 + 892.5;
         }
-        else if (adjustedIncome <=87850){
+        else if (adjustedIncome <= 87850){
             correctTax = (adjustedIncome - 36250) * .25 + 4991.25;
         }
         else if (adjustedIncome > 87850){
             correctTax = (adjustedIncome - 87850) * .28 + 17891.25;
         }
+        }
     }
     else 
+        adjustedIncome = adjustedIncome - 3900 - 3900 - 6100 -6100;
+        if (adjustedIncome > 0){
        if (adjustedIncome <= 17850){
             correctTax = adjustedIncome * .1;
         }
@@ -44,15 +49,21 @@ int main(){
         else if (adjustedIncome > 72500){
             correctTax = (adjustedIncome - 72500) * .28 + 9982.5;
         }
+        }
       
      cout << "Name: " << firstName << " " << lastName << endl;
      cout << "Total adjusted gross income: " << adjustedIncome << endl;
-     cout << "Total tax owed: " << correctTax << endl;
      
-    if (taxWithheld > correctTax) {
+     
+     if (adjustedIncome < 0){
+         cout << "No tax owed." << endl;
+     }
+    else if (taxWithheld > correctTax) {
+        cout << "Total tax owed: " << correctTax << endl;
         cout << firstName << " " << lastName << " is entitled to a refund of $" << taxWithheld - correctTax << endl;
     } 
     else {
+        cout << "Total tax owed: " << correctTax << endl;
         cout << firstName << " " << lastName <<  " will have to pay $" << correctTax - taxWithheld << " more in taxes" << endl;
     }
     
