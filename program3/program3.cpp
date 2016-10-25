06f23581e837616f728ab86785e2c358f314eb06
Zoe Bishop <zbishop001@student.butte.edu>
2016-10-25 00:28:09 +0000

    program3 submission

--- program3/program3.cpp
+++ program3/program3.cpp
@@ -11,113 +11,109 @@ using namespace std;
     Created By: Zoe Bishop
     */
     
-/*Function that compares the user's input to 
-    the computer's choice to decide if the user
-    won or lost the round
-    */
-int game(int userValue){
-    int gameResult;
-    int ties;
-    srand(time(0));
-    int compValue = (rand() % 3) + 1;
-    
-    //If there is a tie it does not add or subtract from the user's score
-    if (userValue == compValue){
-        ties = ties + 1;
-        cout << "The computer picked the same one as you!" << endl;
-        cout << "It's a tie!" << endl;
-    }
-    //Decides if user won or lost if they choose rock
-    if (userValue == 1){
-        if (compValue == 2){
-            gameResult = gameResult;
-            cout << "Computer picked paper. You picked rock." << endl;
-            cout << "You lost this round!" << endl;
-        }
-        else if (compValue == 3){
-            gameResult = gameResult + 1;
-            cout << "Computer picked scissors. You picked rock." << endl;
-            cout << "You won this round!" << endl;
-        }
-    }
-    //Decides if user won or lost if they choose paper
-    if (userValue == 2){
-        if (compValue == 1){
-            gameResult = gameResult + 1;
-            cout << "Computer picked rock. You picked paper." << endl;
-            cout << "You won this round!" << endl;
-        }
-        else if (compValue == 3){
-            gameResult = gameResult;
-            cout << "Computer picked scissors. You picked paper." << endl;
-            cout << "You lost this round!" << endl;
-        }
-    }
-    //Decides if user won or lost if they choose scissors
-    if (userValue == 3){
-        if (compValue == 1){
-            gameResult = gameResult;
-            cout << "Computer picked rock. You picked scissors." << endl;
-            cout << "You lost this round!" << endl;
-        }
-        else if (compValue == 2){
-            gameResult = gameResult + 1;
-            cout << "Computer picked paper. You picked scissors." << endl;
-            cout << "You won this round!" << endl;
-        }
-    }
-    return gameResult + ties;
-}
+class Game{
+    public: 
+        Game();
+        int play();
+        string numToChoice(int choice);
+        int gameRound(int roundNumber);
+    private:
+        int wins;
+        int ties;
+};
 
-int main(){
-    int totalRounds;
-    int userChoice;
-    int gameResult;
-   
+Game::Game(){
+        srand(time(0));
+        wins = 0;
+        ties = 0;
+}
+    
+int Game::play(){
+    int numRounds = 0;
+    int roundResult = 0;
     cout << "Time to play Rock, Paper, Scissors." << endl; 
     cout << "To begin, please select if you would like to play 3, 5, or 7 rounds." << endl;
-    cin >> totalRounds;
-    
-    cout << endl << "First round: Please select 1 for rock, 2 for paper, or 3 for scissors." << endl;
-    cin >> userChoice;
-    gameResult = gameResult + game(userChoice);
-   
-    cout << endl << "Second round: Please select 1 for rock, 2 for paper, or 3 for scissors." << endl;
-    cin >> userChoice;
-    gameResult = gameResult + game(userChoice);
-   
-    cout << endl << "Third round: Please select 1 for rock, 2 for paper, or 3 for scissors." << endl;
-    cin >> userChoice;
-    gameResult = gameResult + game(userChoice);
-    
-    //Adds two more rounds if the user chose 5 or 7 rounds
-    if(totalRounds == 5 || totalRounds == 7){
-        cout << endl << "Fourth round: Please select 1 for rock, 2 for paper, or 3 for scissors." << endl;
-        cin >> userChoice;
-        gameResult = gameResult + game(userChoice);
-       
-        cout << endl << "Fifth round: Please select 1 for rock, 2 for paper, or 3 for scissors." << endl;
-        cin >> userChoice;
-        gameResult = gameResult + game(userChoice);
+    cin >> numRounds;
+    for (int i = 0; i < numRounds; ++i){
+        roundResult = gameRound(i);
+        if(roundResult == 0){
+            ties = ties + 1;
+        }
+        else if(roundResult == 1){
+            wins = wins + 1;
+        }
     }
-    
-    //Adds two more rounds if the user chose 7 rounds
-    if(totalRounds == 7){
-         cout << endl << "Sixth round: Please select 1 for rock, 2 for paper, or 3 for scissors."<< endl;
-        cin >> userChoice;
-        gameResult = gameResult + game(userChoice);
-        
-        cout << endl << "Seventh round: Please select 1 for rock, 2 for paper, or 3 for scissors." << endl;
-        cin >> userChoice;
-        gameResult = gameResult + game(userChoice);
+    string sWins = "";
+    string sTies = "";
+    if(wins != 1){
+        sWins = "s";
     }
-    
-    cout << endl << "You won and tied a total of " << gameResult << " games." << endl;
-    cout << "That is " << gameResult << " out of " << totalRounds << " games." << endl;
-    if (gameResult >= (.5 * totalRounds)){
+    if(ties != 1){
+        sTies = "s";
+    }
+    cout << "You won " << wins << " round" << sWins << " and tied " << ties << " round" << sTies << "." << endl;
+    cout << "That is " << wins + ties << " out of " << numRounds << "." << endl;
+    int totalScore = wins + ties;
+    if (totalScore >= (.5 * numRounds)){
         cout << "You won!" << endl;
     }
-    else if (gameResult < (.5 * totalRounds)){
+    else if (totalScore < (.5 * numRounds)){
         cout << "You lost!" << endl;
     }
 }
+
+string Game::numToChoice(int choice){
+    switch(choice){
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }
+}
+int Game::gameRound(int roundNumber){
+    int userChoice = 0;
+    int compChoice = (rand() % 3) + 1;
+    int roundResult = -2;
+    cout << "Round " << roundNumber + 1 << endl;
+    cout << "Please enter 1 for rock, 2 for paper, and 3 for scissors." << endl;
+    cin >> userChoice;
+    if (userChoice > compChoice || (userChoice == 1 && compChoice == 3)){
+        roundResult = 1;
+    }
+    else if (userChoice == compChoice){
+        roundResult = 0;
+    }
+    else{
+        roundResult = -1;
+    }
+    cout << "You chose " << numToChoice(userChoice) << "." << endl;
+    cout << "The computer chose " << numToChoice(compChoice) << "." << endl;
+    switch(roundResult){
+        case -1: 
+            cout << "You lost!" << endl;
+            break;
+        case 0:
+            cout << "It's a tie!" << endl;
+            break;
+        case 1: 
+            cout << "You won!" << endl;
+            break;
+    }
+    cout << endl;
+    return roundResult;
+}
+
+int main(){
+    Game userGame;
+    userGame.play();
+}
+
+
+    
+    
+  
