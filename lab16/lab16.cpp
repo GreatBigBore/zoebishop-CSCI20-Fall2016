28c379cb908e35ecef8979637beb0b369fb045a9
Zoe Bishop <zbishop001@student.butte.edu>
2016-10-27 19:41:09 +0000

    lab16 submission

--- lab16/lab16.cpp
+++ lab16/lab16.cpp
@@ -30,26 +30,26 @@ int main(){
     totalOrder[9] = 0;
     
     double price[10][2];
-    totalOrder[0][0] = 15;
-    totalOrder[0][1] = 10;
-    totalOrder[1][0] = 4.5;
-    totalOrder[1][1] = 10;
-    totalOrder[2][0] = 3.65;
-    totalOrder[2][1] = 10;
-    totalOrder[3][0] = 12;
-    totalOrder[3][1] = 10;
-    totalOrder[4][0] = 8.75;
-    totalOrder[4][1] = 10;
-    totalOrder[5][0] = 7;
-    totalOrder[5][1] = 10;
-    totalOrder[6][0] = 8.75;
-    totalOrder[6][1] = 10;
-    totalOrder[7][0] = 3.5;
-    totalOrder[7][1] = 10;
-    totalOrder[8][0] = 3;
-    totalOrder[8][1] = 10;
-    totalOrder[9][0] = 12;
-    totalOrder[9][1] = 10;
+    price[0][0] = 15;
+    price[0][1] = 10;
+    price[1][0] = 4.5;
+    price[1][1] = 10;
+    price[2][0] = 3.65;
+    price[2][1] = 10;
+    price[3][0] = 12;
+    price[3][1] = 10;
+    price[4][0] = 8.75;
+    price[4][1] = 10;
+    price[5][0] = 7;
+    price[5][1] = 10;
+    price[6][0] = 8.75;
+    price[6][1] = 10;
+    price[7][0] = 3.5;
+    price[7][1] = 10;
+    price[8][0] = 3;
+    price[8][1] = 10;
+    price[9][0] = 12;
+    price[9][1] = 10;
     
      cout << "Welcome to the fat cafe! Here is our list of menu items:" << endl;
     
@@ -62,17 +62,16 @@ int main(){
     cin >> userOrder;
     totalOrder[userOrder - 1] = totalOrder[userOrder - 1] + 1;
     cout << "That will be $"<< price[userOrder - 1][0] << "." << endl;
-    int totalPrice = price[userOrder - 1][0];
-    cout << "There are " << price[1][userOrder - 1] - 1 << " " << menu[userOrder - 1] << " left." << endl;
+    double totalPrice = price[userOrder - 1][0];
+    cout << "There are " << price[userOrder - 1][1] - 1 << " " << menu[userOrder - 1] << " left." << endl;
     
    while(userOrder != 0){
     cout << endl <<  "Anything else?" << endl;
     cin >> userOrder;
+    if(userOrder == 0)break;
     cout << "That will be $"<< price[userOrder - 1][0] << "." << endl;
     totalPrice = totalPrice + price[userOrder - 1][0];
-    cout << "There are " << price[1][userOrder - 1] - 1 << " " << menu[userOrder - 1] << " left." << endl;
-    totalOrder[userOrder - 1] = totalOrder[userOrder - 1] + 1;
-    
+    cout << "There are " << price[userOrder - 1][1] - 1 << menu[userOrder - 1] << " left." << endl;
    }
    
    for(int j = 0; j < 10; ++j){
