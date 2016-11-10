c1a350932a71c09332d6efd7cd641a9b2441f0c4
Zoe Bishop <zbishop001@student.butte.edu>
2016-11-10 19:54:23 +0000

    program4 submission

--- program4/program4.cpp
+++ program4/program4.cpp
@@ -3,6 +3,15 @@
 #include<fstream>
 using namespace std;
 
+/* This program is a concordance. It accepts an input file
+and compares it to a list of stop words. Any words that are 
+not on the stop word list will be printed out with the
+line number of the first occurence and how many times that
+word was used.
+Created on: 11.10.16
+Created by: Zoe Bishop
+*/
+
 class concord{
     public:
     stopword();
@@ -11,29 +20,27 @@ class concord{
     private:
 };
 
-string newWord(){
+string lineToWords(string newWord){
     int wordStartIndex = 0;
     int wordEndIndex = 0;
     
-    for(int i = 0; i < 571; ++i){
-        getline(testfile, inputWord);
-        for(int j = 0; j < 571; ++j){
-            while(wordStartIndex != inputword.length()){
-                wordEndIndex = find(' ');
-                substr(wordStartIndex, wordEndIndex - wordStartIndex);
-            
-                wordStartIndex = wordEndIndex + 1;
-         }
-     } 
+    while(!testfile.eof){
+        getline(testfile, inputLine);
+        while(wordStartIndex != inputLine.length()){
+            wordEndIndex = find(' ');
+            newWord = substr(wordStartIndex, wordEndIndex - wordStartIndex);
+            wordStartIndex = wordEndIndex + 1;
+        }
+    }
 }
 
-int stopWord(){
+int stopWordCheck(stopWord[]){
      if(!(stopWord[i] == inputWord)){
         ++wordCount[i];
     }
 }
 
-int dupWord(){
+int dupWordCheck(newWord[], wordCount[]){
     if(newWord[i] = inputWord;){
         ++wordCount[i];
         //LINE NUMBER
@@ -44,32 +51,38 @@ int dupWord(){
 int main(){
     concord game;
     
+    //Opens stop word file
     string stop = "stoptxt.txt";
     string stopWord[571];
     
     ifstream stoptxt;
     stoptxt.open(stop);
   
-  for(int i = 0; i < 571; ++i){
+    //Stop words into a stop word array
+    for(int i = 0; i < 571; ++i){
       getline(stoptxt, stopWord[i]);
-  }
+    }
   
-  string input = "testfile.txt";
-  string inputWord;
+    //Opens test file
+    string input = "testfile.txt";
+    string inputWord;
   
-  ifstream testfile;
-  testfile.open(input);
+    ifstream testfile;
+    testfile.open(input);
   
-  string newWords[50];
-  int wordCount[50];
-  int lineNum[50];
+    //Other stuff
+    string newWords[50];
+    int wordCount[50];
+    int lineNum[50];
   
-  for(int i = 0; i < 50; ++i){
+    //Runs functions
+    for(int i = 0; i < 50; ++i){
       game.newWord;
       game.stopWord;
       game.dupWord;
-  }
-
+    }
+    
+    //Prints results
     for(int i = 0; i < 50; ++i){
         cout << newWord[i] << "First occured on line: " << lineNum[i];
         cout << " and occured a total number of " << wordCount[i] << " times." << endl;
