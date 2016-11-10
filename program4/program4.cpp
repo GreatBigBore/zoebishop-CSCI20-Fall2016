0fdb3c6a1e35adcb5922ee93615a9a5944fe4ce2
Zoe Bishop <zbishop001@student.butte.edu>
2016-11-10 19:31:38 +0000

    program4 submission

--- program4/program4.cpp
+++ program4/program4.cpp
@@ -3,53 +3,74 @@
 #include<fstream>
 using namespace std;
 
-/*class concord{
+class concord{
     public:
     private:
 };
-*/
+
+
+int stopWord{
+     if(!(stopWord[i] == inputWord)){
+        ++wordCount[i];
+    }
+}
+
+int dupWord{
+    if(newWord[i] = inputWord;){
+        ++wordCount[i];
+        //LINE NUMBER
+        find(newWord[i]);
+    }
+}
+
+newWord{
+    int wordStartIndex = 0;
+    int wordEndIndex = 0;
+    
+    for(int i = 0; i < 571; ++i){
+        getline(testfile, inputWord);
+        for(int j = 0; j < 571; ++j){
+            while(char != ' '){
+                wordEndIndex = find(' ');
+                substr(wordStartIndex, wordEndIndex - wordStartIndex);
+            
+                wordStartIndex = wordEndIndex + 1;
+         }
+     } 
+}
 
 int main(){
+    concord game;
+    
     string stop = "stoptxt.txt";
-    string stopWords[571];
+    string stopWord[571];
     
     ifstream stoptxt;
     stoptxt.open(stop);
   
   for(int i = 0; i < 571; ++i){
-      getline(stoptxt, stopWords[i]);
-      cout << stopWords[i] << endl;
+      getline(stoptxt, stopWord[i]);
+      cout << stopWord[i] << endl;
   }
   
   string input = "testfile.txt";
-  string inputWords;
+  string inputWord;
   
   ifstream testfile;
   testfile.open(input);
-  
-  for(int i = 0; i < 50; ++i){
-      getline(testfile, inputWords[i]);
-      cout << inputWords[i] << endl;
-  }
   
   string newWords[50];
   int wordCount[50];
   int lineNum[50];
   
-  for(int i = 0; i < 571; ++i){
-      getline(testfile, inputWords);
-    if(!(stopWords[i] == inputWords[i])){
-        ++wordCount[i];
-    }
-    else{
-        newWords[i] = inputWords[i];
-        ++wordCount[i];
-        //LINE NUMBER
-        find(newWords[i]);
-    }
+  for(int i = 0; i < 50; ++i){
+      game.newWord;
+      game.stopWord;
+      game.dupWord;
+  }
 
     for(int i = 0; i < 50; ++i){
-        cout << newWords[i] << "First occured on line: " << lineNum[i];
+        cout << newWord[i] << "First occured on line: " << lineNum[i];
         cout << " and occured a total number of " << wordCount[i] << " times." << endl;
     }
 };
